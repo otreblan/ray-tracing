6c3056f8f114c3e9d09b470e67f31e1c93d1f210
otreblan <otreblain@gmail.com>
2021-06-03 19:59:46 -0500

    Lerp

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,48 @@
 // You should have received a copy of the GNU General Public License
 // along with ray-tracing.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <cmath>
 #include <iostream>
 #include <unistd.h>
 
 #include <fmt/core.h>
 #include <glm/vec3.hpp>
+#include <glm/geometric.hpp>
+#include <glm/gtx/compatibility.hpp>
 
 #include "print.hpp"
+#include "ray.hpp"
+
+glm::vec3 ray_color(const ray& r)
+{
+	glm::vec3 unit_direction = glm::normalize(r.direction);
+	float t = 0.5f*(unit_direction.y + 1.f);
+	return glm::lerp(glm::vec3(1.f, 1.f, 1.f), glm::vec3(0.5f, 0.7f, 1.f), t);
+}
 
 int main()
 {
-	// Image
-	const int image_width = 256;
-	const int image_height = 256;
-
 	bool stdout_tty = isatty(STDOUT_FILENO);
+
+	// Image
+	const float aspect_ratio = 16.f/9.f;
+	const int image_width = 400;
+	const int image_height = image_width/aspect_ratio;
+
+	// Camera
+	float viewport_height = 2.f;
+	float viewport_width = aspect_ratio*viewport_height;
+	float focal_length = 1.f;
+
+	glm::vec3 origin(0.f, 0.f, 0.f);
+	glm::vec3 horizontal(viewport_width, 0.f, 0.f);
+	glm::vec3 vertical(0.f, viewport_height, 0.f);
+	glm::vec3 lower_left_corner =
+		origin -
+		horizontal/2.f -
+		vertical/2.f -
+		glm::vec3(0.f, 0.f, focal_length)
+	;
 
 	// Render
 	fmt::print("P3\n{} {}\n255\n", image_width, image_height);
@@ -40,13 +67,12 @@ int main()
 
 		for (int i = 0; i < image_width; ++i)
 		{
-			glm::vec3 pixel_color(
-				((float)i)/(image_width-1),
-				((float)j)/(image_height-1),
-				0.25f
-			);
+			float u = ((float)i)/(image_width-1);
+			float v = ((float)j)/(image_height-1);
 
-			fmt::print("{}\n", pixel_color);
+			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+
+			fmt::print("{}\n", ray_color(r));
 		}
 	}
 }
