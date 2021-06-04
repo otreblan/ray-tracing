e7c96757c4284cd5ada07998a94275eaf4b35c45
otreblan <otreblain@gmail.com>
2021-06-03 19:08:45 -0500

    Header guard

--- src/print.hpp
+++ src/print.hpp
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with ray-tracing.  If not, see <http://www.gnu.org/licenses/>.
 
+#pragma once
+
 #include <string_view>
 
 #include <fmt/format.h>
